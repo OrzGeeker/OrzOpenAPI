d2b6ccb6862d7a6a98faecbcffcdfa5b637d8dea
wangzhizhou <wangzhizhou@bytedance.com>
2024-08-08 12:34:05 +0800

    refine code struct

--- Sources/Server/OrzServer.swift
+++ Sources/Server/OrzServer.swift
@@ -1,30 +1,9 @@
 import Foundation
 import Vapor
+import OpenAPI
+import OpenAPIVapor
 import Fluent
 import FluentSQLiteDriver
-import OpenAPIRuntime
-import OpenAPIVapor
-import OpenAPI
-
-// Define a type that conforms to the generated protocol.
-struct GreetingServiceAPIImpl: APIProtocol {
-    func getGreeting(
-        _ input: Operations.getGreeting.Input
-    ) async throws -> Operations.getGreeting.Output {
-        let name = input.query.name ?? "Stranger"
-        let greeting = Components.Schemas.Greeting(message: "Hello, \(name)!")
-        return .ok(.init(body: .json(greeting)))
-    }
-
-
-    func getEmoji(
-        _ input: Operations.getEmoji.Input
-    ) async throws -> Operations.getEmoji.Output {
-        let emojis = "👋👍👏🙏🤙🤘"
-        let emoji = String(emojis.randomElement()!)
-        return .ok(.init(body: .plainText(.init(emoji))))
-    }
-}
 
 @main
 struct OrzServer {
