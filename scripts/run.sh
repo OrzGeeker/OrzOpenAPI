cd026265845d678eb5777b0c2eda38cbeb765058
wangzhizhou <wangzhizhou@bytedance.com>
2024-09-04 16:48:48 +0800

    build and run as product

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,5 @@
 WORKING_DIR=$(git rev-parse --show-toplevel)
 cd $WORKING_DIR || exit
 
+swift build -c release -Xswiftc -cross-module-optimization && \
 swift run server serve --hostname 0.0.0.0 --port 8080
